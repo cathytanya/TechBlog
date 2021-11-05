42ce0da57b137630829921ff013e8f71df50877c
cathytanya <cathycolette.tanya@gmail.com>
2021-11-05 13:32:42 -0400

    made more user seeds and made them unique

--- seeds/user-seeds.js
+++ seeds/user-seeds.js
@@ -3,10 +3,38 @@ const { User } = require('../models');
 // the seeds will embed into the USER model
 const userData = [
     {
-        username: "nziclavera",
-        twitter: "july31",
+        username: "nziclvavera",
+        twitter: "judly31",
         github: "nziclavera",
         email: "nziclaverab@gmail.com",
+        password: "cati22cara"
+    },
+    {
+        username: "cathytanya",
+        twitter: "coolcats22",
+        github: "caticole",
+        email: "caticole81@gmail.com",
+        password: "WildnOut"
+    },
+    {
+        username: "jamescooper",
+        twitter: "jamesbond",
+        github: "alexcooper21",
+        email: "jamiebaby@gmail.com",
+        password: "iLikeSuits"
+    },
+    {
+        username: "nellynelson",
+        twitter: "mariahcareysson",
+        github: "singertoscammer",
+        email: "nellynel@gmail.com",
+        password: "WhatIsLove"
+    },
+    {
+        username: "kuntiiscool",
+        twitter: "manstillnothot",
+        github: "singhislion",
+        email: "ksignh@gmail.com",
         password: "cati22cara"
     }
 ]
