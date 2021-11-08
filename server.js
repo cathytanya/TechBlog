4a51e188bb239576dcc90570ce9839f0951441ad
cathytanya <cathycolette.tanya@gmail.com>
2021-11-08 13:01:25 -0500

    heroku

--- server.js
+++ server.js
@@ -3,13 +3,12 @@ const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require('path');
-
 const helpers = require('./utils/helpers')
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({helpers});
 const session = require('express-session');
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3306;
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const sess = {
     secret : 'secret',
